8c2099d27be08d11a33ad756abc81f70554afe8d
sonu <srssoumyaranjansahu123@gmail.com>
2024-05-18 10:23:11 +0530

    cors

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const app = express()
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://cms-client-two.vercel.app/',
     credentials: true,
 }));
 
